5bf9a606915bcfff6e8e006c808162bd19ec6990
gul-najeeb <najeeb@ommo.ai>
2024-09-26 20:40:10 +0200

    signUpForm WIP.

--- src/views/backend/Auth/CreateUser.js
+++ src/views/backend/Auth/CreateUser.js
@@ -1,14 +1,186 @@
-import { getDarkMode } from "../../../store/mode";
+import React, { useState } from "react";
+import { Container, Col, Row, Button, Form } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";  // Import useNavigate
+import Card from "../../../components/Card";
 import { connect } from "react-redux";
+import { getDarkMode } from "../../../store/mode";
+//img
+import logo from "../../../assets/images/logo.png";
+import darklogo from "../../../assets/images/logo-dark.png";
 
 function mapStateToProps(state) {
-    return {
-        darkMode: getDarkMode(state)
-    };
-};
+  return {
+    darkMode: getDarkMode(state),
+  };
+}
 
 const CreateUser = (props) => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [password, setPassword] = useState("");
+  const [companyId, setCompanyId] = useState("");
+  const [roleId, setRoleId] = useState("");
+  const [userType, setUserType] = useState("");
+  const [error , setError] = useState(null);
+  const navigate = useNavigate();
 
+  const handleCreateUser = async () => {
+    let item = { username, email, phone, password, companyId, roleId, userType };
+
+    try {
+      let result = await fetch("http://localhost:5055/api/user/create-user", {
+        method: 'POST',
+        body: JSON.stringify(item),
+        headers: {
+          "Content-Type": 'application/json',
+          "Accept": 'application/json'
+        }
+      });
+
+      result = await result.json();
+      console.log("Create User result:", result);
+
+      if (result.success) {
+        navigate("/dashboard");
+      } else {
+        setError("User not created.");
+      }
+    } catch (error) {
+      console.error("Error during creating a user:", error);
+      setError("An error occurred during user creation. Please try again.");
+    }
+  };
+
+  return (
+    <>
+      <section className="login-content">
+        <Container className="h-100">
+          <Row className="align-items-center justify-content-center h-100">
+            <Col md="5">
+              <Card className="p-3">
+                <Card.Body>
+                  <div className="auth-logo">
+                    <img
+                      src={logo}
+                      className={`img-fluid  rounded-normal  ${!props.darkMode ? "d-none" : ""}`}
+                      alt="logo"
+                    />
+                    <img
+                      src={darklogo}
+                      className={`img-fluid  rounded-normal  ${props.darkMode ? "d-none" : ""}`}
+                      alt="logo"
+                    />
+                  </div>
+                  <h3 className="mb-3 font-weight-bold text-center">Create User</h3>
+                  <div className="mb-5">
+                    <p className="line-around text-secondary mb-0">
+                      <span className="line-around-1">Enter user details</span>
+                    </p>
+                  </div>
+                  <Form>
+                    <Row>
+                      <Col lg="12">
+                        <Form.Group>
+                          <Form.Label className="text-secondary">
+                            Username
+                          </Form.Label>
+                          <Form.Control
+                            type="email"
+                            placeholder="Enter Username"
+                            onChange={(e) => setUsername(e.target.value)}
+                          />
+                        </Form.Group>
+                        <Form.Group>
+                          <Form.Label className="text-secondary">
+                            Email
+                          </Form.Label>
+                          <Form.Control
+                            type="email"
+                            placeholder="Enter Email"
+                            onChange={(e) => setEmail(e.target.value)}
+                          />
+                        </Form.Group>
+                        <Form.Group>
+                          <Form.Label className="text-secondary">
+                            Phone
+                          </Form.Label>
+                          <Form.Control
+                            type="email"
+                            placeholder="Enter Phone Number"
+                            onChange={(e) => setPhone(e.target.value)}
+                          />
+                        </Form.Group>
+                        <Form.Group>
+                          <Form.Label className="text-secondary">
+                            Password
+                          </Form.Label>
+                          <Form.Control
+                            type="password"
+                            placeholder="Enter Password"
+                            onChange={(e) => setPassword(e.target.value)}
+                          />
+                        </Form.Group>
+                        <Form.Group>
+                          <Form.Label className="text-secondary">
+                            Company ID
+                          </Form.Label>
+                          <Form.Control
+                            type="name"
+                            placeholder="Enter Company ID"
+                            onChange={(e) => setCompanyId(e.target.value)}
+                          />
+                        </Form.Group>
+                        <Form.Group>
+                          <Form.Label className="text-secondary">
+                            Role ID
+                          </Form.Label>
+                          <Form.Control
+                            type="name"
+                            placeholder="Enter Role ID"
+                            onChange={(e) => setRoleId(e.target.value)}
+                          />
+                        </Form.Group>
+                        <Form.Group>
+                          <Form.Label className="text-secondary">
+                            User Type
+                          </Form.Label>
+                          <Form.Control
+                            type="email"
+                            placeholder="Enter User Type"
+                            onChange={(e) => setUserType(e.target.value)}
+                          />
+                        </Form.Group>
+                      </Col>
+                      <Col lg="12" className="mt-2">
+                      </Col>
+                    </Row>
+                    {/* Display error message if login fails */}
+                    {error && <p className="text-danger">{error}</p>}
+
+                    {/* Remove <Link> and replace it with Button */}
+                    <Button
+                      className="btn btn-primary btn-block mt-2"
+                      onClick={handleCreateUser}
+                    >
+                      Log In with Email/Phone
+                    </Button>
+
+                    <Col lg="12" className="mt-3">
+                      <p className="mb-0 text-center">
+                        Don't have a company?{" "}
+                        <Link to="/auth/sign-up">Create Company Account.</Link>
+                      </p>
+                    </Col>
+                  </Form>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </>
+  );
 };
 
 export default connect(mapStateToProps)(CreateUser);
