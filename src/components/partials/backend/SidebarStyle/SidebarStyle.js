9fe5c64b1581eb14cb442ff135d311423087387c
najeeb1023-ommo <najeeb@ommo.ai>
2024-10-08 23:47:34 +0200

    Layout fixed of sidenav bar.

--- src/components/partials/backend/SidebarStyle/SidebarStyle.js
+++ src/components/partials/backend/SidebarStyle/SidebarStyle.js
@@ -84,37 +84,78 @@ const SidebarStyle = (props) => {
 
             return (
                 <li className="sidebar-layout" key={menuKey}>
-                    <Accordion.Toggle as={Button} eventKey={menuKey} variant=" collapsed" data-toggle="collapse" aria-expanded={activeMenu === '0' ? 'true' : 'false'} >
-                        <i>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="18" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
-                                    </svg>
-                        </i>
-                        <span className="ml-2">{menuKey}</span>
-                        <svg xmlns="http://www.w3.org/2000/svg" className="svg-icon iq-arrow-right arrow-active" width="15" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7" />
+                  <div className="d-flex align-items-center">
+                    {/* Link to the main menu path */}
+                    <Link to={`/${basePath}${menuKey.toLowerCase()}`} className="svg-icon flex-grow-1">
+                      <span className="ml-2">{menuKey}</span> {/* Main menu link */}
+                    </Link>
+              
+                    {/* Only show the arrow if there are subcategories */}
+                    {subMenu.length > 0 && (
+                      <Accordion.Toggle
+                        as={Button}
+                        eventKey={menuKey}
+                        variant="link"
+                        className="p-0 ml-2"
+                        onClick={(e) => e.stopPropagation()} // Prevents the main link from being clicked when the arrow is clicked
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="15"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M9 5l7 7-7 7"
+                          />
                         </svg>
-                    </Accordion.Toggle>
+                      </Accordion.Toggle>
+                    )}
+                  </div>
+              
+                  {/* Submenu items */}
+                  {subMenu.length > 0 && (
                     <Accordion.Collapse className="submenu" eventKey={menuKey}>
-                        <ul className="submenu">
-                            {subMenu.map((item) => (
-                                <li key={item}>
-                                    <Link to={`/${basePath}${item.toLowerCase()}`} className="svg-icon">
-                                        <i className="">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="18" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 012 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" />
-                                            </svg>
-                                        </i>
-                                        <span className="ml-2">{item}</span>
-                                    </Link>
-                                </li>
-                            ))}
-                        </ul>
+                      <ul className="submenu">
+                        {subMenu.map((item) => (
+                          <li key={item}>
+                            <Link
+                              to={`/${basePath}${item.toLowerCase()}`} // Dynamic link for subcategories
+                              className="svg-icon"
+                            >
+                              <i>
+                                <svg
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  width="18"
+                                  fill="none"
+                                  viewBox="0 0 24 24"
+                                  stroke="currentColor"
+                                >
+                                  <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 012 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"
+                                  />
+                                </svg>
+                              </i>
+                              <span className="ml-2">{item}</span> {/* Submenu item text */}
+                            </Link>
+                          </li>
+                        ))}
+                      </ul>
                     </Accordion.Collapse>
+                  )}
                 </li>
-            );
-        });
-    };
+              );
+              
+              
+            })
+        };
 
     return (
         <>
