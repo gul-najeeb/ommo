413e581b7aa45a5ee8133d871c23eb40173d6bad
OnlineSourceS <115408642+CodeWith-HAMZA@users.noreply.github.com>
2024-10-16 21:48:27 +0500

    .env setting

--- src/services/index.js
+++ src/services/index.js
@@ -1,11 +1,10 @@
 import axios from "axios";
- 
-// Create an Axios instance
+import { baseUrl } from "../constants";
+
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5055", // Replace with your API base URL
-  timeout: 10000, // Optional: Set a timeout (in milliseconds)
+  baseURL: baseUrl, // further soon will come through .env
+  timeout: 10000,
   headers: {
     "Content-Type": "application/json",
-    // Add any other headers you need here
   },
 });
